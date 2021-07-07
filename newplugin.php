ac15eb8881f209b918d3ab2617e1b34617a866ed
Al-amintpi <alamincmt7418@gmail.com>
2021-07-07 18:33:20 +0600

    plugin first commit

---
+++ newplugin.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: First Plugin
+Plugin URI: https://coderit.fun/
+Description: First Plugin.
+Version: 1.1.0
+Author: Alamin
+Author URI: https://coderit.fun/
+License: GPLv2 or later
+Text Domain: coderitfun
+*/
